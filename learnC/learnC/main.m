78766a7fe0d9abb31602ab891fc8f9675f3494de
qq573092608 <1466081601@qq.com>
2017-07-17 12:36:19 +0800

    C语言
    C语言学习

---
+++ learnC/learnC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  learnC
+//
+//  Created by 黄小刚 on 2017/7/17.
+//  Copyright © 2017年 黄小刚. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
